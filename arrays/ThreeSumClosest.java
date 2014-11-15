dfec670ec1f46ea3e7c04caf81ccd512517e236f
tianlinliu <tianlin@Tianlins-MBP.att.net>
2014-11-15 14:55:41 -0800

    first 11-15

--- arrays/ThreeSumClosest.java
+++ arrays/ThreeSumClosest.java
@@ -1,21 +1,33 @@
 public class ThreeSumClosest {
     public int threeSumClosest(int[] num, int target) {
-        if (num == null || num.length < 3) return -1;  //?
+        if (num == null || num.length < 3) return -1;
 
+        Arrays.sort(num);
         int result = 0;
-        for (int i = 0; i < num - 2; i++) {
-            int low = i + 1;
-            int high = num.length - 1;
-            while (low < high) {
-                int sum = num[i] + num[low] + num[high];
-                int diff = Math.abs(sum - target) - Math.abs(result - target);
-                if (diff == 0) {
+        int minDiff = Integer.MAX_VALUE;
+        for (int i = 0; i < num.length - 2; i++) {
+            int left = i + 1;
+            int right = num.length - 1;
+            while (left < right) {
+                int sum = num[i] + num[left] + num[right];
+                if (sum == target) {
                     return target;
-                } else if (diff < 0) {
-                    result = sum;
+                } else if (sum < target) {
+                    if (target - sum < minDiff) {
+                        minDiff = target - sum;
+                        result = sum;
+                    }
+                    left++;
+                } else {
+                    if (sum - target < minDiff) {
+                        minDiff = sum - target;
+                        result = sum;
+                    }
+                    right--;
                 }
             }
         }
 
+        return result;
     }
 }
